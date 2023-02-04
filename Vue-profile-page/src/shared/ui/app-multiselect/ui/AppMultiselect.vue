96fd7a3b802fd90da61c351a722da0328b05059f
Anatoly Ryazanov <Ryazanov.AO@yandex.ru>
2023-02-04 18:26:51 +0600

    style: refactor styles

--- Vue-profile-page/src/shared/ui/app-multiselect/ui/AppMultiselect.vue
+++ Vue-profile-page/src/shared/ui/app-multiselect/ui/AppMultiselect.vue
@@ -159,12 +159,11 @@ const filteredOptions = computed(() => {
   position: relative;
 
   .backdrop-transparent {
+    @include size(100%);
     z-index: 3;
     position: fixed;
     top: 0;
     left: 0;
-    width: 100%;
-    height: 100%;
   }
 
   .multiselect {
--- Vue-profile-page/src/shared/ui/input-group/ui/InputGroup.vue
+++ Vue-profile-page/src/shared/ui/input-group/ui/InputGroup.vue
@@ -94,12 +94,12 @@ watch(inputValue, () => {
   }
 
   &__input {
+    width: 100%;
     height: 40px;
     border-radius: 5px;
     padding: 0 12px;
     box-shadow: 0 12px 16px rgba(0, 0, 0, 0.02);
     border: 1px solid $base-grey-blue;
-    width: 100%;
 
     @include MIN_1200 {
       height: 48px;
